27efe621e4e0c0c5497eeec86e4106ced12df59a
= <lord66tin66@gmail.com>
2020-11-01 19:23:06 +0100

    bad performance with colliding ConvexGeometry, change to box geometry

--- three_react_tst_2_cannon/src/App.js
+++ three_react_tst_2_cannon/src/App.js
@@ -5,7 +5,7 @@ import { softShadows, MeshWobbleMaterial, OrbitControls } from "drei";
 import { useSpring, a } from "react-spring/three";
 import { Physics, usePlane, useBox, useConvexPolyhedron } from '@react-three/cannon'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry'
+import { ConvexBufferGeometry, ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry'
 
 import './App.css';
 
@@ -65,29 +65,29 @@ function Cube(props) {
   )
 }
 
-function ModelTst(props) {
-  const { nodes } = useLoader(GLTFLoader, '/shape2TstModel.glb')
-  const geo = useMemo(() => {
-    const g = new THREE.Geometry().fromBufferGeometry(nodes.Cube.geometry)
-    // Merge duplicate vertices resulting from glTF export.
-    // Cannon assumes contiguous, closed meshes to work
-    g.mergeVertices()
-    // Ensure loaded mesh is convex and create faces if necessary
-    return new ConvexGeometry(g.vertices)
-  }, [nodes])
+// function ModelTst(props) {
+//   const { nodes } = useLoader(GLTFLoader, '/shape2TstModel.glb')
+//   const geo = useMemo(() => {
+//     const g = new THREE.Geometry().fromBufferGeometry(nodes.Cube.geometry)
+//     // Merge duplicate vertices resulting from glTF export.
+//     // Cannon assumes contiguous, closed meshes to work
+//     g.mergeVertices()
+//     // Ensure loaded mesh is convex and create faces if necessary
+//     return new ConvexGeometry(g.vertices)
+//   }, [nodes])
 
-  const [ref] = useConvexPolyhedron(() => ({ mass: 1000,  ...props, args: geo }))
-  return (
-    <mesh castShadow receiveShadow ref={ref} geometry={nodes.Cube.geometry} {...props} dispose={null}>
-      {/* <bufferGeometry /> */}
-      <meshStandardMaterial attach="material"  />
-    </mesh>
-  )
-}
+//   const [ref] = useConvexPolyhedron(() => ({ mass: 1000,  ...props, args: geo }))
+//   return (
+//     <mesh castShadow receiveShadow ref={ref} geometry={nodes.Cube.geometry} {...props} dispose={null}>
+//       {/* <bufferGeometry /> */}
+//       <meshStandardMaterial attach="material"  />
+//     </mesh>
+//   )
+// }
 
 
 function ModelTst2(props) {
-  const { nodes } = useLoader(GLTFLoader, '/tinModelsTst1.glb')
+  const { nodes } = useLoader(GLTFLoader, './tinModelsTst1.glb')
   const geo = useMemo(() => {
     const g = new THREE.Geometry().fromBufferGeometry(nodes.[props.modelId].geometry)
     // Merge duplicate vertices resulting from glTF export.
@@ -106,11 +106,44 @@ function ModelTst2(props) {
   )
 }
 
+function ModelTst3(props) {
+  const { nodes } = useLoader(GLTFLoader, './tinModelsTst1.glb')
+  const geo = useMemo(() => {
+    const g = new THREE.Geometry().fromBufferGeometry(nodes.[props.modelId].geometry)
+    // Merge duplicate vertices resulting from glTF export.
+    // Cannon assumes contiguous, closed meshes to work
+    g.mergeVertices()
+    // Ensure loaded mesh is convex and create faces if necessary
+    return new ConvexGeometry(g.vertices)
+  }, [nodes])
+  // console.log()
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], ...props, args:[5,2,2], }))
+    // const [ref] = useConvexPolyhedron(() => ({ mass: props.mass,material:{friction:1e10,restitution:0.01},   ...props, args: geo }))
+  console.log(geo)
+  return (
+    <mesh ref={ref} geometry={nodes.[props.modelId].geometry} {...props} >
+      {/* <boxBufferGeometry attach="geometry" args={[10,2,2]} /> */}
+      {/* <ConvexGeometry  */}
+      <meshStandardMaterial attach="material"  color="blue" />
+
+      {/* <ConvexBufferGeometry */}
+    </mesh>
+  )
+
+  // const [ref] = useConvexPolyhedron(() => ({ mass: props.mass, material:{friction:1e10,restitution:0.01},  ...props, args: geo }))
+  // return (
+  //   <mesh castShadow receiveShadow ref={ref} geometry={nodes.[props.modelId].geometry} {...props} dispose={null}>
+  //     <bufferGeometry attach="geometry" />
+  //     <meshStandardMaterial attach="material"  />
+  //   </mesh>
+  // )
+}
+
 function App() {
   return (
     <>
     <Canvas 
-      shadowMap 
+      // shadowMap 
       sRGB 
       gl={{ alpha: false }} 
       camera={{ position: [-1, 2, 5], fov: 50 }}
@@ -135,9 +168,9 @@ function App() {
       
       <Physics
         // gravity={[0, 0, 30]}
-        step={1/30}
+        step={1/60}
         iterations={4}
-        allowSleep={true}
+        allowSleep={false}
         // defaultContactMaterial={{
         //   contactEquationStiffness: 1e8,
         //   friction:1e8,
@@ -159,7 +192,7 @@ function App() {
         <Cube position={[2,5,-2]} />
         <Cube position={[2,7,-2]} />  */}
 
-         <Wall position={[2.5,15,0]} />
+         {/* <Wall position={[2.5,15,0]} /> */}
 {/*
         <Cube position={[-2,9,-2]} />
         <Cube position={[-2,12,-2]} />
@@ -170,9 +203,11 @@ function App() {
         <Cube position={[-2,112,-2]} />
         <Cube position={[-2,51,-2]} />
         <Cube position={[-2,71,-2]} /> */}
-        <ModelTst2 modelId={"Projects"} position={[5, 5, -1.5]} mass={100} />
-        <ModelTst2 modelId={"Skills"} position={[5, 7, 0]} mass={100} />
-        <ModelTst2 modelId={"Contact"} position={[5, 10, -2]} mass={100} />
+        {/* <ModelTst3 modelId={"Projects"} position={[-5, 10, -1.5]} mass={100} /> */}
+        {/* <ModelTst2 modelId={"Skills"} position={[5, 3, 0]} mass={100} /> */}
+        {/* <ModelTst3 modelId={"Contact"} position={[5, 6, -2]} mass={100} /> */}
+        <ModelTst3 modelId={"Contact"} position={[5, 10, -2]} mass={100} />
+        <ModelTst3 modelId={"Contact"} position={[5, 6, -2]} mass={100} />
       </Physics>
       {/* <Physics> */}
         {/* <Plane /> */}
