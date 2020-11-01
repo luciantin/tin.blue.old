62f545291e7489803eb75c9680fe51a71b7d3701
= <lord66tin66@gmail.com>
2020-11-01 18:09:26 +0100

    canon deploy test

--- three_react_tst_2_cannon/src/App.js
+++ three_react_tst_2_cannon/src/App.js
@@ -24,6 +24,19 @@ function Plane(props) {
   )
 }
 
+function Wall({position, color}){
+  const [ref] = useBox(() => ({args:[3,30,30], mass: 500, material:{friction:100.0,restitution:1.01}, position: position}))
+  // useFrame(()=> {console.log(ref.current)})
+  // (ref.current.rotation.x = ref.current.rotation.y += 0.04)
+  return (
+    <mesh ref={ref}
+    receiveShadow
+    castShadow>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
 
 function SpinningMesh({position, args, color, speed}){
   const mesh = useRef(null)
@@ -38,7 +51,7 @@ function SpinningMesh({position, args, color, speed}){
 }
 
 function Cube(props) {
-  const [ref] = useBox(() => ({ mass: 1, position: props.position, }))
+  const [ref] = useBox(() => ({ mass: 500, material:{friction:1e20,restitution:0.01}, position: props.position, }))
   // useFrame(()=> {console.log(ref.current)})
   // (ref.current.rotation.x = ref.current.rotation.y += 0.04)
   return (
@@ -63,9 +76,30 @@ function ModelTst(props) {
     return new ConvexGeometry(g.vertices)
   }, [nodes])
 
-  const [ref] = useConvexPolyhedron(() => ({ mass: 100, ...props, args: geo }))
+  const [ref] = useConvexPolyhedron(() => ({ mass: 1000,  ...props, args: geo }))
   return (
     <mesh castShadow receiveShadow ref={ref} geometry={nodes.Cube.geometry} {...props} dispose={null}>
+      {/* <bufferGeometry /> */}
+      <meshStandardMaterial attach="material"  />
+    </mesh>
+  )
+}
+
+
+function ModelTst2(props) {
+  const { nodes } = useLoader(GLTFLoader, '/tinModelsTst1.glb')
+  const geo = useMemo(() => {
+    const g = new THREE.Geometry().fromBufferGeometry(nodes.[props.modelId].geometry)
+    // Merge duplicate vertices resulting from glTF export.
+    // Cannon assumes contiguous, closed meshes to work
+    g.mergeVertices()
+    // Ensure loaded mesh is convex and create faces if necessary
+    return new ConvexGeometry(g.vertices)
+  }, [nodes])
+
+  const [ref] = useConvexPolyhedron(() => ({ mass: props.mass, material:{friction:1e10,restitution:0.01},  ...props, args: geo }))
+  return (
+    <mesh castShadow receiveShadow ref={ref} geometry={nodes.[props.modelId].geometry} {...props} dispose={null}>
       {/* <bufferGeometry /> */}
       <meshStandardMaterial attach="material"  />
     </mesh>
@@ -99,21 +133,46 @@ function App() {
           shadow-camera-bottom={-10}
           />
       
-      <Physics>
+      <Physics
+        // gravity={[0, 0, 30]}
+        step={1/30}
+        iterations={4}
+        allowSleep={true}
+        // defaultContactMaterial={{
+        //   contactEquationStiffness: 1e8,
+        //   friction:1e8,
+        //   restitution:1e8,
+        //   contactEquationRelaxation:1e8,
+        //   frictionEquationRelaxation:1e8,
+        //   frictionEquationStiffness:1e8,
+        // }}
+      >
         <Plane />
-        <ModelTst position={[1, 5, 0]} rotation={[0.4, 0.1, 0.1]} />
-        <Cube position={[0,7,-2]} />
+        {/* <ModelTst position={[1, 5, 0]} rotation={[0.4, 0.1, 0.1]} /> */}
+        {/* <Cube position={[0.2,7,-2]} />
         <Cube position={[0,9,-2]} />
         <Cube position={[0,12,-2]} />
         <Cube position={[0,5,-2]} />
-        <Cube position={[2,9,-2]} />
+        */}
+        {/* <Cube position={[2,9,-2]} />
         <Cube position={[2,12,-2]} />
         <Cube position={[2,5,-2]} />
-        <Cube position={[2,7,-2]} />
+        <Cube position={[2,7,-2]} />  */}
+
+         <Wall position={[2.5,15,0]} />
+{/*
         <Cube position={[-2,9,-2]} />
         <Cube position={[-2,12,-2]} />
         <Cube position={[-2,5,-2]} />
         <Cube position={[-2,7,-2]} />
+        
+        <Cube position={[-2,91,-2]} />
+        <Cube position={[-2,112,-2]} />
+        <Cube position={[-2,51,-2]} />
+        <Cube position={[-2,71,-2]} /> */}
+        <ModelTst2 modelId={"Projects"} position={[5, 5, -1.5]} mass={100} />
+        <ModelTst2 modelId={"Skills"} position={[5, 7, 0]} mass={100} />
+        <ModelTst2 modelId={"Contact"} position={[5, 10, -2]} mass={100} />
       </Physics>
       {/* <Physics> */}
         {/* <Plane /> */}
